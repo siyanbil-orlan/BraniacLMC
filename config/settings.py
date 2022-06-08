1a9763ddcc13afaaeb10df01f9620d4fcf46c8b4
Siyanbil Orlan <siyanbil.orlan@gmail.com>
2022-06-09 02:30:01 +0700

    lesson_7 Logging

--- config/settings.py
+++ config/settings.py
@@ -169,3 +169,31 @@ SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = '557975242185-dkl0qpnjsi5mro7p0mn70v98kef3qtt6.a
 SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = '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'
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+LOG_FILE = BASE_DIR / "var" / "log" / "main_log.log"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "console": {
+            "format": "[%(asctime)s] %(levelname)s %(name)s (%(lineno)d) %(message)s"
+        },
+    },
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": LOG_FILE,
+            "formatter": "console",
+        },
+        "console": {"class": "logging.StreamHandler", "formatter": "console"},
+    },
+    "loggers": {
+        "django": {"level": "INFO", "handlers": ["console"]},
+        "mainapp": {
+            "level": "DEBUG",
+            "handlers": ["file"],
+        },
+    },
+}
